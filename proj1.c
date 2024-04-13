e060e4722ed996161a9cafffd782fdb1bb1b12f1
unknown <vrwankhade24@gmail.com>
2024-04-13 11:48:44 +0530

    added to main branch

--- proj1.c
+++ proj1.c
@@ -11,3 +11,13 @@ int sum (int a, int b)
 {
     return a+b;
 }
+#adding diff() function
+int diff(int a, int b)
+{
+    rreturn a-b;
+}
+#adding mul() function
+int mul (int a, int b)
+{
+    return a*b;
+}
